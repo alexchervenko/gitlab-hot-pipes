47b560a1a5f28af72a7937050401caeaab83cd7f
Alexey Chervenko <83578747+alexchervenko@users.noreply.github.com>
2025-04-23 10:58:49 +0300

    add token and ref to form

--- hot_pypes_start.py
+++ hot_pypes_start.py
@@ -3,12 +3,16 @@ from config_parser import init_properties
 
 config_props, form_data  = init_properties()
 url = config_props["base_uri"] + config_props["start_endpoint"].format(project_id=config_props["project.id"])
+form_data["ref"] = config_props["branch"]
+form_data["token"] = config_props["trigger_token"]
 var_to_override = None
 second_var_to_override = None
 
 while(not stand_name):
     var_to_override = input("Input data: ")
     second_var_to_override = input("Input data: ")
+    form_data["variables[VAR]"] = var_to_override
+    form_data["variables[VAR]"] = second_var_to_override
     print("\n")
 
 print(f"Starting {var_to_override} {second_var_to_override}\n")
